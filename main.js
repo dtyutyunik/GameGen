93818ca58b036c40fbab8cef9b95d86165c7d32f
Dmitriy Tyutyunik <dmitriy.tyutyunik@gmail.com>
2019-01-26 09:59:53 -0500

    adding files

---
+++ main.js
@@ -0,0 +1,18 @@
+var config= {
+  type: Phaser.AUTO,
+  width: 800,
+  height:800,
+  color: 'red',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 200
+      }
+    }
+  },
+  scene: [Stage1]
+};
+var game = new Phaser.Game(config);
+
+
