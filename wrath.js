b6f780eb848f0a390911b4c34f521d2074448ca3
Dmitriy Tyutyunik <dmitriy.tyutyunik@gmail.com>
2019-01-27 13:28:00 -0500

    spirte movedment works

--- wrath.js
+++ wrath.js
@@ -21,7 +21,7 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-let old,old2,player,cam;
+let old,old2,player,cam,walkRight,walkLeft,sprite;
 
 function random(speed){
   var r= Math.floor(Math.random() * speed+3) *50;
@@ -37,6 +37,10 @@ function preload(){
   this.load.image('red', 'assets/red.gif');
   this.load.image('green', 'assets/green.gif');
   this.load.image('background', 'assets/[Wrath] Street destroyed.png');
+
+  // this.load.atlas("atlas", "assets/atlas_name.png", "assets/atlas_name_atlas.json");
+
+  this.load.spritesheet('right', 'assets/right.png', { frameWidth: 400, frameHeight: 400 });
 
 }
 
@@ -67,115 +71,77 @@ function create(){
   this.cameras.main.setBounds(0, 0, 2200, 655);
   this.physics.world.setBounds(0, 120, 1600, 560);
 
-
-  // pic = this.add.image(0, -120, 'background').setOrigin(0, 0);
   pic = this.add.image(0, 0, 'background').setOrigin(0, 0);
   pic2=this.add.image(1200, 400, 'background');
-  // console.log(pic.height);
-  console.log(pic);
-  console.log(pic2);
+
+
   this.cameras.main.setViewport(0, 0, 1400, 900);
-  // this.add.image(, 760, 'background');
+
 
 
   let gravity=game.config.physics.arcade.gravity;
 
-  // this.cameras.main.setSize(game.config.width,game.config.height);
+
 
   cam=this.cameras.main;
-  // cam.duration=300;
-  console.log(cam.shakeEffect);
-
-  // this.world.setBounds(0, 0, 1920, 1200);
-
-  player = this.physics.add.sprite(0, 142, 'player');
-
-  player.displayWidth=110;
-  player.body.setSize(200, 200, 50, 50);
-  player.scaleY=player.scaleX;
-  player.body.offset.x+=130;
-  player.body.offset.y+=10;
-  //player is not effected by gravity by reversing its gravity
-  player.body.gravity.x=50;
-
-  //y--142 top
-  //y--655 bottom
-  //player.x -->1582
-  //x--31.5 left
 
 
-// console.log(this.player.onWorldBounds);
+  var walkingConfig = {
+       key: 'walkRight',
+       frames: this.anims.generateFrameNumbers('right'),
+       frameRate: 20,
+       // yoyo: true,
+       repeat: 0
+   };
+
+anim = this.anims.create(walkingConfig);
+player = this.physics.add.sprite(0, 142, 'right');
+
+   player.anims.load('walkRight');
+
+   player.displayWidth=110;
+   player.body.setSize(200, 200, 50, 50);
+   player.scaleY=player.scaleX;
+   player.body.offset.x+=130;
+   player.body.offset.y+=10;
+   player.body.gravity.x=50;
+   player.body.gravity.x=50;
 
 
 
-
-  // for(let i=0;i<20;i++){
-  //   old2 = this.physics.add.sprite(850, random(10), 'red');
-  //   // old2.displayWidth=50;
-  //   // old2.scaleY=old2.scaleX;
-  //
-  //   old2.body.gravity.x=-random(2);
-  // }
   game.canvas.offsetWidth=1600;
-  console.log(game)
 
-  // this.game.config.world.setBounds(0, 0, 3500, this.game.height);
-
-  // this.physics.add.collider(player, old2);
-
-
-
-  //declare its own set of gavity;
-  // old2.body.gravity.x=-150;
   game.canvas.clientWidth=2000;
-  console.log(player.getBounds());
-  console.log(player.Container);
-  // player.
-  // old2.setCollideWorldBounds(true);
-  // old.setCollideWorldBounds(true);
 
-  // need to udpate to move within this
+
+
   player.setCollideWorldBounds(true);
 
-  console.log()
-  // Phaser.Physics.Arcade.World.setBounds(0, 0, 1500, 800, true,true,true,true);
-// console.log(player.setCollideWorldBounds(true));
-
-  // console.log(old)
-
-// console.log(game.config.width);
-// console.log(player.x)
 
 
-      this.key_A=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-      this.key_W=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-      this.key_S=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-      this.key_D=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-
-// this.cameras.main.startFollow(player);
-// this.cameras.fade(0x000000, 4000);
 
 
-// this.cameras.main.setBounds(-200,0,game.config.width+200,game.config.height);
-// this.cameras.main.setScroll(player.x);
-// this.cameras.main.startFollow(player);
-// this.cameras.main.startFollow(player, 0.7, 0.7);
+      // this.key_A=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+      // this.key_W=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+      //
+      // this.key_S=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+      // this.key_D=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
- // this.cameras.main.setBounds(0, 0, 1200, 800);
-  this.cameras.main.startFollow(player, true);
-  this.cameras.main.followOffset.set(-200, 0);
+
+      this.cameras.main.startFollow(player, true);
+      this.cameras.main.followOffset.set(-100, 0);
 
 
 }
 function update(delta){
 
 let frequency=Math.floor(delta)
-
-  if(frequency%60==10){
+// console.log(frequency%60);
+  if(frequency%60==0){
 
     old=this.physics.add.sprite(850, random(11), 'green');
     old2=this.physics.add.sprite(1580, random(11), 'red');
-    // console.log(frequency);
+    // console.log(frequency%60);
    // old2=this.physics.add.sprite(850, random(11), 'red');
    // old=this.physics.add.sprite(1580, random(11), 'green');
 
@@ -205,12 +171,16 @@ this.physics.add.overlap(player, old, ouch, null, this);
 if(player.x>=1560){
   console.log('you got to the end')
 }
+ let cursors = this.input.keyboard.createCursorKeys();
+// console.log(cursorKeys);
 
-      if(this.key_W.isDown){
-        player.y-=3;
-       }
-      if(this.key_S.isDown){player.y+=3;}
-      if(this.key_A.isDown){player.x-=3;}
-      if(this.key_D.isDown){player.x+=3;}
+// console.log(player.anims)
+      if(cursors.up.isDown){player.y-=3;player.anims.play('walk',true);}
+      if(cursors.down.isDown){player.y+=3; player.anims.play('walk',true)}
+      if(cursors.left.isDown){player.x-=3;player.anims.play('walk',true)}
+      if(cursors.right.isDown){player.x+=3;player.anims.play('walk',true);}
+    else{
+    // player.anims.play('walk');
+     }
 
 }
